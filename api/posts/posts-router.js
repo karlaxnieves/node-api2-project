9c3d8df79fc19599cf1340eb3a40e52967e4c054
Karla Nieves <karlaxnieves@gmail.com>
2021-08-03 16:11:18 -0700

    first get is finished

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,5 +4,30 @@ const Posts = require('./posts-model')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts)
+    })    
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,  
+        })
+    })
+})
+
+router.get('/', (req, res) => {
+   
+})
+
+router.post('/', (req, res) => {
+   
+})
+
+router.put('/', (req, res) => {
+ 
+})
 
 module.exports = router 
